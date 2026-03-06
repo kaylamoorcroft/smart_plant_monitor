0b54443ca9417ebbfd2e9734146fffbc0182e99f
k-ayls <kayleeasmith04@hotmail.com>
2026-03-06 14:44:15 -0400

    feat: barrel file for importing pages + file name changes

---
+++ lib/all_libraries.dart
@@ -0,0 +1,9 @@
+//barrel file to export all pages found in app for routing
+
+export 'package:smart_plant_monitor/Pages/history.dart';
+export 'package:smart_plant_monitor/Pages/humid_graph.dart';
+export 'package:smart_plant_monitor/Pages/light_graph.dart';
+export 'package:smart_plant_monitor/Pages/plant_info.dart';
+export 'package:smart_plant_monitor/Pages/soil_graph.dart';
+export 'package:smart_plant_monitor/Pages/temp_graph.dart';
+
