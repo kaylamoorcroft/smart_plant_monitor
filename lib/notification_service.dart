10c671cd4f0e4b2e98c6a98a85ffe59de023d2f3
kaylamoorcroft <kayla.moorcroft@icloud.com>
2026-04-07 17:38:02 -0300

    fix: notifications crashing web build

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,13 +1,21 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class NotificationService {
   final FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   //Ask for permission to send notifications
   Future<void> initFCM() async {
+    // Notifications don't work on web, exit so app doesn't crash
+    if (kIsWeb) {
+      print("Push notifications not available for web version of app");
+      return;
+    }
+
     await messaging.requestPermission();
 
+    // Also doesn't work on mac / ios. Prevent crash here too
     if (Platform.isMacOS || Platform.isIOS) {
       // Free developer accounts will always get 'null' here
       String? apnsToken = await messaging.getAPNSToken();
